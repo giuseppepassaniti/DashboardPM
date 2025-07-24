181bcebab301cbe1b479d448bd7a1b9944c8b2c0
giuseppepassaniti <giuseppepassaniti00@gmail.com>
2025-07-24 16:35:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Questa riga dice a Vite di usare il plugin React
+  // per capire e compilare correttamente i file .jsx
+  plugins: [react()],
+})
